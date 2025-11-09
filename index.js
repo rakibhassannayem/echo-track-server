ae83397c8ba689a448b8b6b7fbd38c06acee5392
rakibhassannayem <rakibhassannayem@gmail.com>
2025-11-09 09:55:04 +0600

    get challenges data from db

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8vhjke1.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8vhjke1.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,10 +21,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    
 
+    const db = client.db("echo-track_DB");
+    const challengeCollection = db.collection("challenges");
 
-    
+    app.get("/challenges", async (req, res) => {
+      const result = await challengeCollection.find().toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
