f49c764400b7fd21ff790756169b4f0c541422e4
rakibhassannayem <rakibhassannayem@gmail.com>
2025-11-09 09:11:57 +0600

    setup express and cors

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Echo Track server is running fine!");
+});
+
+app.listen(port, () => {
+  console.log(`Sever is listening on port ${port}`);
+});
